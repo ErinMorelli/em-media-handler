4bf6f897e967ee14881165c449c811b2dfea628d
Erin Morelli <erin@erinmorelli.com>
2014-09-10 16:52:13 -0400

    Cleaning up files

--- video/movies.py
+++ video/movies.py
@@ -34,11 +34,7 @@ class Rename:
 		self.strict = "-non-strict"
 		self.analytics = "-no-analytics"
 		# Local paths
-		self.tvPath = 'Television'
 		self.movPath = 'Movies'
-		# Beet Options
-		self.beet = "/usr/local/bin/beet"
-		self.beetslog = "beetslog.log"
 
 
 	def __getInfo(self, mFormat, mDB):
@@ -68,39 +64,6 @@ class Rename:
 		return new_file
 
 
-	def episodeHandler(self):
-		logging.info("Starting episode information handler")
-		# Set Variables
-		tvFormat = "%s/{n}/Season {s}/{n.space('.')}.{'S'+s.pad(2)}E{e.pad(2)}" % self.tvPath
-		tvDB = "thetvdb"
-		# Get info
-		new_file = self.__getInfo(tvFormat, tvDB)
-		logging.debug("New file: %s", new_file)
-		# Check for failure
-		if new_file == None:
-			return None
-		# Set search query
-		ePath = re.escape(self.tvPath)
-		tvFind = "%s\/(.*)\/(.*)\/.*\.S\d{2,4}E(\d{2,3}).\w{3}" % ePath
-		logging.debug("Search query: %s", tvFind)
-		# Extract info
-		episode = re.search(tvFind, new_file)
-		if episode == None:
-			return None
-		# Show title
-		show_name = episode.group(1)
-		# Season 
-		season = episode.group(2)
-		# Episode 
-		ep_num = episode.group(3)
-		ep_num_fix = re.sub('^0', '', ep_num)
-		episode = "Episode %s" % ep_num_fix
-		# Set title
-		ep_title = "%s (%s, %s)" % (show_name, season, episode)
-		# Return Show Name, Season, Episode (file)
-		return ep_title, new_file
-
-
 	def movieHandler(self):
 		logging.info("Starting movie information handler")
 		# Set Variables
@@ -125,71 +88,3 @@ class Rename:
 		# Return Movie Title
 		return mov_title, new_file
 
-
-	def musicHandler(self, isSingle=False):
-		logging.info("Starting music information handler")
-		# Set Variables
-		if isSingle:
-			mType = "Song"
-			mTags = "-sql"
-			mQuery = "Tagging track\:\s(.*)\nURL\:\n\s{1,4}(.*)\n\((Similarity\: .*%)\)"
-		else:
-			mType = "Album"
-			mTags = "-ql"
-			mQuery = "(Tagging|To)\:\n\s{1,4}(.*)\nURL\:\n\s{1,4}(.*)\n\((Similarity\: .*%)\)"
-		# Set up query
-		mCMD = [self.beet,
-			"import", self.file,
-			mTags, self.beetslog,
-		]
-		logging.debug("Query: %s", mCMD)
-		# Process query
-		p = Popen(mCMD, stdout=PIPE, stderr=PIPE)
-		# Get output
-		(output, err) = p.communicate()
-		logging.debug("Query output: %s", output)
-		logging.debug("Query return errors: %s", err)
-		# Process output
-		if err != '':
-			return True, err
-		# Extract Info 
-		musicFind = re.compile(mQuery)
-		logging.debug("Search query: %s", mQuery)
-		# Format data
-		musicData = musicFind.search(output)
-		musicInfo = "%s: %s" % (mType, musicData.group(2))
-		logging.info("MusicBrainz URL: %s" % musicData.group(3))
-		logging.info(musicData.group(4))
-		# Return music data
-		return False, musicInfo
-
-
-class Extract:
-	def __init__(self, filename):
-		logging.info("Initializing extraction class")
-		self.file = filename
-		self.filebot = "/usr/bin/filebot"
-
-
-	def fileHandler(self):
-		logging.info("Getting files from compressed folder")
-		# Set up query
-		mCMD = [self.filebot, 
-			"-extract", self.file
-		]
-		logging.debug("Query: %s", mCMD)
-		# Process query
-		p = Popen(mCMD, stdout=PIPE)
-		# Get output
-		(output, err) = p.communicate()
-		logging.debug("Query output: %s", output)
-		logging.debug("Query return errors: %s", err)
-		# Process output
-		fileinfo = re.search(r"Extracting files \[(.*)\]\n", output)
-		if fileinfo == None:
-			return None
-		extracted = fileinfo.group(1)
-		# Break into array
-		new_files = extracted.split(", ")
-		# Return array of extracted files
-		return new_files
