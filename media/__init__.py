81ea3ea5436108e40fdc0e4dcef2e88f3397487d
Erin Morelli <erin@erinmorelli.com>
2014-09-10 19:48:18 -0400

    Configuring submodules

---
+++ media/__init__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+#
+# This file is a part of EM Media Handler
+# Copyright (c) 2014 Erin Morelli
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+
+__version__ = '1.0'
+__author__ = 'Erin Morelli'
